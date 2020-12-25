1b6b54f7e615cb7894158d3a987d553cb9a009df
YousufSSyed <42646062+YousufSSyed@users.noreply.github.com>
2020-12-25 17:36:58 -0500

    Update SearchProcess.py

--- SearchProcess.py
+++ SearchProcess.py
@@ -3,35 +3,39 @@ import time, sys, os, pickle, io
 api = API('e4588190-ab7d-43d3-b488-3716aac0272d')
 
 oldtags = ["Miscellaneous"]
-newtags = ["Google Search"]
+newtags = ["Google Searches"]
 
 searchfile = open('search.pkl', 'rb')
 bookmarks = pickle.load(searchfile)
 print("Loaded data file.")
 
-def update():
+for index, bookmark in enumerate(bookmarks):
+  if bookmark is None:
+    print("Bookmark #" + str(index) + ": already processed.")
+    continue
+  print("Bookmark #" + str(index) + ". URL: " + bookmark.link + ". Title: " + bookmark.title + ". Tags: " + str(bookmark.tags))
+  bookmarktags = newtags
+  for index2, bookmark2 in enumerate(bookmarks[index + 1:]):
+    if bookmark.link == bookmark2.link and index != index2:
+      bookmarktags += bookmark2.tags + bookmark.tags
+      bookmarktags = [tag for tag in bookmarktags if tag not in oldtags]
+      bookmarks[index] = None
+      remove = Raindrop.remove(api, id=bookmark.id)
+      newbookmark = Raindrop.update(api, id=bookmark2.id, tags=bookmarktags, collection=bookmark2.collection, created=bookmark2.created)
+      searchfile.close()
+      searchfile = open('search.pkl', 'wb')
+      pickle.dump(bookmarks, searchfile, pickle.HIGHEST_PROTOCOL)
+      searchfile.close()
+      searchfile = open('search.pkl', 'rb')
+      print("Bookmark #" + str(index) + ": Duplicate bookmark found. Bookmarks combined and updated.")
+      break
+  bookmarktags += [tag for tag in bookmark.tags if tag not in oldtags]
+  print(str(bookmarktags))
+  newbookmark = Raindrop.update(api, id=bookmark.id, tags=bookmarktags, collection=bookmark.collection, created=bookmark.created)
+  bookmarks[index] = None
   searchfile.close()
   searchfile = open('search.pkl', 'wb')
   pickle.dump(bookmarks, searchfile, pickle.HIGHEST_PROTOCOL)
   searchfile.close()
   searchfile = open('search.pkl', 'rb')
-
-for index, bookmark in enumerate(bookmarks):
-  print("Bookmark #" + str(index) + ". URL: " + bookmark.link + ". Title: " + bookmark.title + ". Tags: " + str(bookmark.tags))
-  if len([tag for tag in bookmark.tags if tag in newtags]) != 0 or bookmark is None:
-    bookmarks[index] = None
-    continue
-  bookmarktags = newtags
-  for index2, bookmark2 in enumaterate(bookmarks[index + 1:]):
-    if bookmark.link == bookmark2.link and index1 != index2:
-      bookmarktags += bookmark2.tags + bookmark1.tags
-      bookmarktags = [tag for tag in bookmarktags if tag not in oldtags]
-      bookmarks[index] = None
-      remove = Raindrop.remove(api, id=bookmark.id)
-      newbookmark = Raindrop.update(api, id=bookmark2.id, tags=bookmarktags)
-      update()
-      break
-  bookmarktags += [tag for tag in bookmark.tags if tag not in oldtags]
-  newbookmark = Raindrop.update(api, id=bookmark.id, tags=bookmarktags)
-  bookmarks[index] = None
-  update()
+  print("Bookmark #" + str(index) + " updated.")
