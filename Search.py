293d4add659c301b6abd552af23d74c62a227592
YousufSSyed <42646062+YousufSSyed@users.noreply.github.com>
2021-02-17 17:00:12 -0500

    Update Search.py

--- Search.py
+++ Search.py
@@ -1,24 +1,30 @@
 from raindropio import *
+from rdfunctions import *
 import time, sys, os, pickle
 api = API('e4588190-ab7d-43d3-b488-3716aac0272d')
 
-searchterm = "https://www.google.com/search?client=firefox"
+searchterm = "reddit.com"
+searchtag = "Miscellaneous Reads"
+limit = True
 
 searchfile = open('search.pkl', 'wb')
 page = 0
 bookmarks = []
-limit = True
-pagelimit = 19
+count = 0
 if limit:
-  while page <= pagelimit:
-    bookmarks = bookmarks + Raindrop.search(api, page=page, collection=CollectionRef({"$id": 0}), word=searchterm, perpage=sys.maxsize)
-    print("Added bookmarks from page" + str(page))
+  while (items:=Raindrop.search(api, page=page, collection=CollectionRef({"$id": 0}), word=searchterm, perpage=50, tag=searchtag)) and page < 19:
+    bookmarks = bookmarks + items
+    print("Added bookmarks from page " + str(page))
     page += 1
 else:
-  while (items:=Raindrop.search(api, page=page, collection=CollectionRef({"$id": 0}), word=searchterm, perpage=sys.maxsize)):
-    bookmarks = bookmarks + items
-    print("Added bookmarks from page" + str(page))
-    page += 1
+    while (items:=Raindrop.search(api, page=page, collection=CollectionRef({"$id": 0}), perpage=50)):
+      for item in items:
+        if searchterm in item.link and searchtag in item.tags:
+          bookmarks.append(item)
+          count += 1
+          print("Bookmark #" + str(count) + " added. URL: " + item.link + ". Title: " + item.title + ". Tags: " + str(item.tags)) 
+        print("Added bookmarks from page " + str(page))
+        page += 1
 pickle.dump(bookmarks, searchfile, pickle.HIGHEST_PROTOCOL)
 print("Created data file.")
 searchfile.close()
