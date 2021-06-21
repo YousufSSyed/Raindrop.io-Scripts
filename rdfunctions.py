3b82c2393f0ce0f8176989d4d9b4a29aebabba21
Yousuf Syed <42646062+YousufSSyed@users.noreply.github.com>
2021-06-20 21:04:41 -0400

    Update rdfunctions.py

--- rdfunctions.py
+++ rdfunctions.py
@@ -1,17 +1,24 @@
 from raindropio import *
 import sys, time, datetime
 from datetime import datetime
+api = API('e4588190-ab7d-43d3-b488-3716aac0272d')
 
-#Search for existing bookmarks, if they exist, get all the tags from all bookmarks, and update the first bookmark to be in the most frequent collection, and delete all others. Otherwise, create new bookmark
 def addBookmark(url: str, bookmarktags: list, createdtime=None, overwrite=False):
-    api = API('e4588190-ab7d-43d3-b488-3716aac0272d')
     if url == "about:blank":
         return "Invalid URL"
-    createdtime2 = datetime.utcfromtimestamp(createdtime).strftime("%b %d %Y %-I:%M %p") if createdtime is not None else None
     createdtime = datetime.utcfromtimestamp(createdtime).strftime('%Y-%m-%d %H:%M:%S') if createdtime is not None else None
-    bookmarks = Raindrop.search(api, page=0, collection=CollectionRef({"$id": 0}), word=url, perpage=sys.maxsize)
+    createdtime2 = datetime.utcfromtimestamp(createdtime).strftime("%b %d %Y %-I:%M %p") if createdtime is not None else None
+    bookmarks = []
+    # Search for if there are existing links
+    try:
+        bookmarks = Raindrop.search(api, page=0, collection=CollectionRef({"$id": 0}), word=url, perpage=sys.maxsize)
+    except:
+        api = API('e4588190-ab7d-43d3-b488-3716aac0272d')
+        bookmarks = Raindrop.search(api, page=0, collection=CollectionRef({"$id": 0}), word=url, perpage=sys.maxsize)
+    # Make a list of duplicate links if they exist
     bookmarks = [bookmark for bookmark in bookmarks if bookmark.link == url or bookmark.link == "https://" + url or bookmark.link == "http://" + url]
     print("URL: " + url + ". New Tags: " + str(bookmarktags) + ". Time Added: " + (createdtime2 if createdtime2 is not None else "none") + ". Current time: " + datetime.now().strftime("%b %d %Y %I:%M %p"))
+    # if existing bookmarks exist, delete extra ones and update the last one.
     if len(bookmarks) != 0:
         if overwrite == False:
             for bookmark in bookmarks:
@@ -21,12 +28,20 @@ def addBookmark(url: str, bookmarktags: list, createdtime=None, overwrite=False)
         if len(bookmarks) > 1:
             for i in range(len(bookmarks) - 1):
                 remove = Raindrop.remove(api, id=bookmarks[i + 1].id)
-        updatedbookmark = Raindrop.update(api, id=bookmarks[0].id, tags=bookmarktags, collection=finalcollection, created=createdtime)
-        return "Existing Bookmark(s) found, tags from existing bookmarks put into new bookmark, and other existing bookmarks have been deleted."
+        try:
+            updatedbookmark = Raindrop.update(api, id=bookmarks[0].id, tags=bookmarktags, collection=finalcollection, created=createdtime)
+        except:
+            api = API('e4588190-ab7d-43d3-b488-3716aac0272d')
+            updatedbookmark = Raindrop.update(api, id=bookmarks[0].id, tags=bookmarktags, collection=finalcollection, created=createdtime)
+        return "Existing Bookmark found. Bookmark updated."
     else:
-        newbookmark = Raindrop.create(api, link=url, tags=bookmarktags, created=createdtime)
+        try:
+            newbookmark = Raindrop.create(api, link=url, tags=bookmarktags, created=createdtime)
+        except:
+            api = API('e4588190-ab7d-43d3-b488-3716aac0272d')
+            newbookmark = Raindrop.create(api, link=url, tags=bookmarktags, created=createdtime)
         return "New bookmark created."
 
-# sleep for 2 seconds (for the sake of the 120 requests per minute)
+# sleep for 1 second (for the sake of the 120 requests per minute limit)
 def rest():
     time.sleep(1)
