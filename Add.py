cdf7699e594658409f3c78a4467f2f5f089aeea4
Yousuf Syed <42646062+YousufSSyed@users.noreply.github.com>
2022-11-01 02:59:14 -0400

    Update Add.py

--- Add.py
+++ Add.py
@@ -1,21 +1,33 @@
-from raindropio import *
 from rdfunctions import *
-from rdsettings import *
-import sys, time
+
+parser = argparse.ArgumentParser("A CLI Script to add raindrop.io bookmarks and specify tags.")
+parser.add_argument("-r", 
+help="A flag so that if a new bookmarks are added with the same URL as exisiting bookmark(s), the existing ones would be deleted.", action="store_true")
+parser.add_argument("-o", help="A flag to add bookmark(s) only once then quit the script immediately.", action="store_true")
+parser.add_argument('-b', help="Specify one bookmark, or multiple delimited by spaces. This will skip the add bookmarks prompt the first time.", nargs='+')
+parser.add_argument("-s", help="A flag to show all of your tags when adding bookmarks", action="store_true")
+parser.add_argument("-t", help="Only show all the bookmark tags and nothing else.", action="store_true")
+args = parser.parse_args()
+
+if args.t:
+    [print(f"{str(tags.index(tag))}. {tag}" ) for tag in tags]
+    quit()
 
 while True:
     # Get URL and tags for new bookmark
     urls = []
-    text = "placeholder"
-    while text != "":
-        text = input("Enter a URL: ")
-        urls.append(text)
-    urls.pop()
-    tagmessage = ""
-    for tag in tags:
-        tagmessage = tagmessage + "\n" + str(tags.index(tag)) + ". " + tag
-    bookmarktaggs = [tags[int(x)] for x in input(tagmessage + "\nEnter numbers in to add tags: ").split()]
+    if args.b is not None: urls = args.b
+    else:
+        text = "placeholder"
+        print("Add bookmarks to your collection. Enter URL(s) in: ")
+        while text != "":
+            text = input()
+            urls.append(text)
+        urls.pop()
+    [print(f"{str(tags.index(tag))}. {tag}" ) for tag in tags]
+    bookmarktaggs = [tags[int(x)] for x in input("Enter numbers in: ").split()]
     #add bookmark, then wait
     for link in urls:
-        print(addBookmark(url=link, bookmarktags=bookmarktaggs))
-        time.sleep(0.5)
+        addBookmark(url=link, bookmarktags=bookmarktaggs, overwrite=args.r)
+    if (args.b is not None) or args.o:
+        break
