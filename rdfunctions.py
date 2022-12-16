b32efeeecc424daedf3c552862197e251d055df9
Yousuf Syed <42646062+YousufSSyed@users.noreply.github.com>
2022-12-16 15:18:17 -0500

    Update rdfunctions.py

--- rdfunctions.py
+++ rdfunctions.py
@@ -1,40 +1,49 @@
 from raindropio import *
 from rdsettings import *
-import sys, time, datetime
+import sys, time, datetime, os, pickle, io, argparse
 from datetime import datetime
 api = API(APIKey)
 
 def addBookmark(url: str, bookmarktags: list, createdtime=None, overwrite=False):
-    if url == "about:blank":
-        return "Invalid URL"
+    if "about:blank" in url: print("Invalid URL"); return
     createdtime = datetime.utcfromtimestamp(createdtime).strftime('%Y-%m-%d %H:%M:%S') if createdtime is not None else None
     createdtime2 = datetime.utcfromtimestamp(createdtime).strftime("%b %d %Y %-I:%M %p") if createdtime is not None else None
-    bookmarks = []
-    # Search for if there are existing links
-    try:
-        bookmarks = Raindrop.search(api, page=0, collection=CollectionRef({"$id": 0}), word=url, perpage=sys.maxsize)
-    except:
-        api = API(APIKey)
-        bookmarks = Raindrop.search(api, page=0, collection=CollectionRef({"$id": 0}), word=url, perpage=sys.maxsize)
-    # Make a list of duplicate links if they exist
-    bookmarks = [bookmark for bookmark in bookmarks if bookmark.link == url or bookmark.link == "https://" + url or bookmark.link == "http://" + url]
-    print("URL: " + url + ". New Tags: " + str(bookmarktags) + ". Time Added: " + (createdtime2 if createdtime2 is not None else "none") + ". Current time: " + datetime.now().strftime("%b %d %Y %I:%M %p"))
-    # if existing bookmarks exist, delete extra ones and update the last one.
+    print(f"Adding URL \"{url}\"{', ' + str(bookmarktags) + ' tags included' if len(bookmarktags) == 0 else ''}{f', and with the custom timestamp: {createdtime2}' if createdtime2 is not None else ''}, at {datetime.now().strftime('%I:%M%p')}.")
+    bookmarks = searchBookmarks(url)
     if len(bookmarks) != 0:
         if overwrite == False:
-            for bookmark in bookmarks:
-                bookmarktags = bookmarktags + bookmark.tags
+            for bookmark in bookmarks: bookmarktags = bookmarktags + bookmark.tags
         collections = [bookmark.collection for bookmark in bookmarks]
         finalcollection = max(set(collections), key = collections.count)
         if len(bookmarks) > 1:
-            for i in range(len(bookmarks) - 1):
-                remove = Raindrop.remove(api, id=bookmarks[i + 1].id)
-        updatedbookmark = Raindrop.update(api, id=bookmarks[0].id, tags=bookmarktags, collection=finalcollection, created=createdtime)
-        return "Existing Bookmark found. Bookmark updated."
+            for i in range(len(bookmarks) - 1): Raindrop.remove(api, id=bookmarks[i + 1].id)
+        Raindrop.update(api, id=bookmarks[0].id, tags=bookmarktags, collection=finalcollection, created=createdtime)
+        print("Existing Bookmark(s) found, merged, and updated." if overwrite == False else "Bookmark updated.")
     else:
-        newbookmark = Raindrop.create(api, link=url, tags=bookmarktags, created=createdtime)
-        return "New bookmark created."
+        try: Raindrop.create(api, link=url, tags=bookmarktags, created=createdtime)
+        except: apiRefresh(); Raindrop.create(api, link=url, tags=bookmarktags, created=createdtime)
+        print("New bookmark created.")
+    rest()
 
-# sleep for 1 second (for the sake of the 120 requests per minute limit)
-def rest():
-    time.sleep(1)
+def searchBookmarks(url: str):
+    bookmarks = []
+    try: bookmarks = Raindrop.search(api, page=0, collection=CollectionRef({"$id": 0}), word=url, perpage=50)
+    except: apiRefresh(); bookmarks = Raindrop.search(api, page=0, collection=CollectionRef({"$id": 0}), word=url, perpage=50)
+    bookmarks = [bookmark for bookmark in bookmarks if bookmark.link == url or bookmark.link in [f"{prefix}{bookmark.link}" for prefix in ["http://", "https://"]]]  
+    rest(); return bookmarks
+
+def deleteBookmark(url: str):
+    print("URL: " + url)
+    bookmarks = searchBookmarks(url)
+    if len(bookmarks) <= 0: print("Bookmark does not exist."); return
+    count = len(bookmarks)
+    for bookmark in bookmarks:
+        try: Raindrop.remove(api, id=bookmark.id)
+        except: apiRefresh(); Raindrop.remove(api, id=bookmark.id)
+    print(f"{str(count)} 'bookmark'{'s' if count > 1 else ''} deleted.")
+    rest()
+
+def apiRefresh(): api = API(APIKey)
+
+# sleep for 0.5 seconds (for the sake of the 120 requests per minute limit)
+def rest(): time.sleep(0.5)
